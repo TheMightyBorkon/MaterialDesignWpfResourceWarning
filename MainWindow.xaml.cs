88d1f82cda2e142b4526f5bdd96411088d3e489e
Steve Rands <steven.rands@trelleborg.com>
2020-05-28 11:39:36 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		void AddButtonClick(object sender, RoutedEventArgs e)
+		{
+			var tb = new TextBox
+			{
+				Text = "Hello",
+				HorizontalAlignment = HorizontalAlignment.Left,
+				VerticalAlignment = VerticalAlignment.Top,
+				Margin = new Thickness(20),
+				Width = 200
+			};
+
+			_grid.Children.Add(tb);
+		}
+
+		void RemoveButtonClick(object sender, RoutedEventArgs e)
+		{
+			_grid.Children.Remove(_grid.Children[0]);
+		}
+	}
+}
